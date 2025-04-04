ff2417df5007d6cd9e0029bf39a1606a286ed794
Mailson <mailson.passos@alumni.usp.br>
2025-04-04 17:00:08 -0300

    update setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ mkdir airflow && \
 pip install -e . && \
 pip install -r requirements_dag.txt && \
 eval $(cat .env) && \
+export AIRFLOW__CORE__LOAD_EXAMPLES=False &&
 airflow db migrate && \
 mv dags airflow
 
